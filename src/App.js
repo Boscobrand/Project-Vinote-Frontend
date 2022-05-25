5a38ae12b6c718d5fa241be2f31cbc69b55fbd6a
Boscobrand <Tony.Bosco@boscobrand.com>
2022-05-25 12:09:51 -0400

    5-25 12pm update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Route, Routes, Link } from "react-router-dom"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import Collect from "./Components/Collect"
+import About from "./Components/About"
+import "./App.css"
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
-}
+            <nav>
+                <Link to ="/">Home</Link>
+                <div>
+                    <Link to="/"><h4>Home</h4></Link>
+                    <Link to="/About"><h4>About</h4></Link>
+                </div>
+            </nav>
+            <Header />
+            <main>
 
+            <Routes>
+  
+                <Route path = "/About/" element={<About />}></Route>
+                <Route path = "/Collect/" element={<Collect />}></Route>
+                {/* <Route path = "/Experience/" element={<Experience />}></Route>
+                <Route path = "/Share/" element={<Share />}></Route> */}
+                <Route path = "/Footer/" element = {<Footer />} />
+                
+                {/* <Route path = "/ResultList/:Recipe" element={<Recipe/>}/> */}
+                {/* <Route path = "/ResultList/" element={<ResultList recipes={recipes}/>}/> */}
+           
+            </Routes>
+            </main>
+        </div>
+
+    );
+}
 export default App;
---
+++ src/Components/WineCard.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+
+// function WineCard(props) {
+     
+//      return(
+
+//          <div className="WineBox">
+//             {props.reviews.map(review => {
+//                 return(
+//                     <div className = "WineCard">
+//                         <div className="rev-title">{review.title}</div>
+//                         <div className="rev-body">{review.body}</div>
+//                         <div className="rev-author">{review.author}</div>
+//                         <div className="rev-rating">{review.rating}</div>
+//                     </div>    
+//                 )
+            
+//             })}
+            
+//         </div>
+
+//      )
+
+// }
+// export default WineCard;

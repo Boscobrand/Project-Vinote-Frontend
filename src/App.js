10d2923652c234e51c4624def68b91df17532b69
Boscobrand <Tony.Bosco@boscobrand.com>
2022-05-25 13:37:12 -0400

    dev 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react"
-import { Route, Routes, Link } from "react-router-dom"
+import React, {Component} from "react"
+import { BrowserRouter as Route, Routes, withRouter, Link } from "react-router-dom"
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { LoginCallback, Security, SecureRoute } from '@okta/okta-react';
+import Secure from './Components/Secure';
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
 import Collect from "./Components/Collect"
@@ -7,10 +10,44 @@ import About from "./Components/About"
 import "./App.css"
 
 
+const oktaAuth = new OktaAuth({
+  issuer: 'https://https://dev-61688829.okta.com/oauth2/default',
+  clientId: '0oa55xgso1NUsEvgS5d7',
+  redirectUri: window.location.origin + '/login/callback'
+});
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.restoreOriginalUri = async (_oktaAuth, originalUri) => {
+      props.history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+    };
+  }
+
+  render() {
+    return (
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={this.restoreOriginalUri}>
+        <Route path="/" exact={true} component={Secure}/>
+        <Route path="/login/callback" component={LoginCallback}/>
+      </Security>
+    );
+  }
+}
+
+const AppWithRouterAccess = withRouter(App);
+
+class RouterApp extends Component {
+  render() {
+    return (<Router><AppWithRouterAccess/></Router>);
+  }
+}
+
+
+
+function AppArrange() {
   return (
-    <div className="App">
+    <div className="AppArrange">
             <nav>
                 <Link to ="/">Home</Link>
                 <div>
@@ -38,4 +75,5 @@ function App() {
 
     );
 }
-export default App;
+export default RouterApp;
+// export default App;
